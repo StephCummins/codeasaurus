1afb81d2e8025975939658f36497c882fe60db51
StephCummins <steph.a.cummins@gmail.com>
2024-01-04 23:03:29 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../public/style.scss';
+import Logo from './components/Logo.jsx';
+
+export default function App() {
+  return (
+    <div>
+      <Logo />
+      <h1>HELLO, WORLD!</h1>;
+    </div>
+  );
+}
